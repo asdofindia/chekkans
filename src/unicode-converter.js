3bfb1147815f5fcf3e60e8ca1c4fb4a18da064c8
Akshay S Dinesh <asdofindia@gmail.com>
2015-10-11 17:49:39 +0530

    add rudimentary map option

--- src/unicode-converter.js
+++ src/unicode-converter.js
@@ -6,11 +6,19 @@ function UnicodeConvertStream(options) {
     if(!(this instanceof UnicodeConvertStream)) {
         return new UnicodeConvertStream(options);
     }
+    this.map = {}
+    var rules = options.map.split('\n');
+    for (var i = 0; i < rules.length; i++) {
+        var rule = rules[i];
+        if rule.startswith("#") continue;
+        [lhs, rhs] = rule.split("=");
+        this.map[lhs]=rhs;
+    }
     Transform.call(this, options);
 }
 
 UnicodeConvertStream.prototype._transform = function (chunk, encoding, callback) {
-    var unicodeChunk = chunk.toString().toUpperCase();
+    var unicodeChunk = this.map[chunk.toString()];
     this.push(unicodeChunk);
     callback();
 }
