37ed1e0266b3b12f80fb468c3d723f580ca87d3b
Akshay S Dinesh <asdofindia@gmail.com>
2015-10-11 18:43:46 +0530

    allow for maps upto 3 char long

--- src/unicode-converter.js
+++ src/unicode-converter.js
@@ -24,14 +24,38 @@ function UnicodeConvertStream(options) {
         var rhs = rule.split('=')[1];
         this.map[lhs]=rhs;
     }
+    this.charBuffer = '';
     Transform.call(this, options);
 }
 
 UnicodeConvertStream.prototype._transform = function (chunk, encoding, callback) {
     chunk = chunk.toString();
-    for (var i = 0; i < chunk.length; i++) {
-        var unicodeChunk = this.map[chunk[i]];
-        this.push(unicodeChunk);
+    this.charBuffer = this.charBuffer + chunk;
+    while (this.charBuffer != '') {
+        if (this.charBuffer.length > 2) {
+            // if there's a mapping for a 3 char slice
+            if (this.map[this.charBuffer.slice(0,3)] != undefined) {
+                this.push(this.map[this.charBuffer.slice(0,3)]);
+                this.charBuffer = this.charBuffer.slice(3);
+                continue;
+            }
+        }
+        if (this.charBuffer.length > 1) {
+            if (this.map[this.charBuffer.slice(0,2)] != undefined) {
+                this.push(this.map[this.charBuffer.slice(0,2)]);
+                this.charBuffer = this.charBuffer.slice(2);
+                continue;
+            }
+        }
+        if (this.charBuffer.length > 0) {
+            if (this.map[this.charBuffer.slice(0,1)] != undefined) {
+                this.push(this.map[this.charBuffer.slice(0,1)]);
+                this.charBuffer = this.charBuffer.slice(1);
+                continue;
+            } else {
+                break;
+            }
+        }
     }
     callback();
 }
