68fe3b6ab595a0f2532823915a08226768f860c9
Akshay S Dinesh <asdofindia@gmail.com>
2015-10-11 19:30:13 +0530

    handle postbase

--- src/unicode-converter.js
+++ src/unicode-converter.js
@@ -24,36 +24,56 @@ function UnicodeConvertStream(options) {
         var rhs = rule.split('=')[1];
         this.map[lhs]=rhs;
     }
+    this.map['\n']='\n';
+    this.map[' ']=' ';
     this.charBuffer = '';
+    this.baseBuffer = '';
+    this.postBase = options.postBase;
     Transform.call(this, options);
 }
 
 UnicodeConvertStream.prototype._transform = function (chunk, encoding, callback) {
+    var push = function(that, char) {
+        if (that.postBase.indexOf(char) >= 0) {
+            that.baseBuffer = that.baseBuffer + char;
+        } else {
+            that.push(char);
+            if (that.baseBuffer != '') {
+                that.push(that.baseBuffer);
+                that.baseBuffer = '';
+            }
+        }
+    }
     chunk = chunk.toString();
     this.charBuffer = this.charBuffer + chunk;
+    console.log(this.charBuffer);
     while (this.charBuffer != '') {
+        this.charBegin = this.charBuffer;
         if (this.charBuffer.length > 2) {
             // if there's a mapping for a 3 char slice
             if (this.map[this.charBuffer.slice(0,3)] != undefined) {
-                this.push(this.map[this.charBuffer.slice(0,3)]);
+                push(this, this.map[this.charBuffer.slice(0,3)]);
                 this.charBuffer = this.charBuffer.slice(3);
                 continue;
             }
         }
         if (this.charBuffer.length > 1) {
             if (this.map[this.charBuffer.slice(0,2)] != undefined) {
-                this.push(this.map[this.charBuffer.slice(0,2)]);
+                push(this, this.map[this.charBuffer.slice(0,2)]);
                 this.charBuffer = this.charBuffer.slice(2);
                 continue;
             }
         }
         if (this.charBuffer.length > 0) {
             if (this.map[this.charBuffer.slice(0,1)] != undefined) {
-                this.push(this.map[this.charBuffer.slice(0,1)]);
+                push(this, this.map[this.charBuffer.slice(0,1)]);
                 this.charBuffer = this.charBuffer.slice(1);
                 continue;
             } else {
-                break;
+                if (this.charBegin == this.charBuffer) {
+                    console.log('could not decode' + this.charBuffer[0]);
+                    this.charBuffer = this.charBuffer.slice(1);
+                }
             }
         }
     }
